964e252a0523579b242ac5d042f220d39a73a196
parksubeom <sooknise@naver.com>
2025-11-28 03:22:12 +0900

    chore : 라벨 경로추가

--- packages/after/src/components/ui/index.ts
+++ packages/after/src/components/ui/index.ts
@@ -9,3 +9,4 @@ export { FormInput } from "./FormInput";
 export { FormSelect } from "./FormSelect";
 export { FormTextarea } from "./FormTextarea";
 export { FormCheckbox } from "./FormCheckbox";
+export { Label } from "./Label";
